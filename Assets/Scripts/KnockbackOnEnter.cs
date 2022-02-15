d57b7f89eca8ba5c7117c9b882297020f4b9700f
Dazirez <dbzheng@umich.edu>
2022-02-15 18:57:48 -0500

    updates to knockback

--- Assets/Scripts/KnockbackOnEnter.cs
+++ Assets/Scripts/KnockbackOnEnter.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+[RequireComponent(typeof(Rigidbody))]
+[DisallowMultipleComponent]
 public class KnockbackOnEnter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float knockback_power = 20f;
+    public bool destroy_self_on_touch = false;
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider other)
     {
-        
+
+        Rigidbody other_rb = other.GetComponent<Rigidbody>();
+
+
+
+        /* Perform Knockback */
+        if (other_rb != null)
+        {
+
+
+            Vector3 knockback_direction = (other.transform.position - transform.position).normalized;
+            other_rb.velocity = Vector2.zero;
+            other_rb.AddForce(knockback_direction * knockback_power, ForceMode.Impulse);
+            ///* Determine Knockback direction -> horizontal vs vertical */
+            //if (Math.Abs(knockback_direction.x) > Math.Abs(knockback_direction.y))
+            //{
+            //    if (knockback_direction.x > 0)
+            //    {
+            //        other_rb.AddForce(new Vector2(knockback_power, 0f), ForceMode.Impulse);
+            //        // other_rb.velocity = new Vector2(knockback_power, 0f);
+
+            //    }
+            //    else
+            //    {
+            //        other_rb.AddForce(new Vector2(-knockback_power, 0f), ForceMode.Impulse);
+            //        // other_rb.velocity = new Vector2(-knockback_power, 0f);
+
+            //    }
+            //}
+            //else
+            //{
+            //    if (knockback_direction.y > 0)
+            //    {
+            //        other_rb.AddForce(new Vector2(0f, knockback_power), ForceMode.Impulse);
+            //        // other_rb.velocity = new Vector2(0f, knockback_power);
+
+            //    }
+            //    else
+            //    {
+            //        other_rb.AddForce(new Vector2(0f, -knockback_power), ForceMode.Impulse);
+            //        // other_rb.velocity = new Vector2(0f, -knockback_power);
+
+            //    }
+            //}
+
+        }
+
+
+        /* Destroy self */
+        if (destroy_self_on_touch)
+            Destroy(gameObject);
     }
 }
