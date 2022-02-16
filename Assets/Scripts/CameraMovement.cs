14a622ec500dc2a98664e135d19e8c8cb28b3398
Dazirez <dbzheng@umich.edu>
2022-02-15 21:36:00 -0500

    camera shake

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class CameraMovement : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset;
+    Subscription<CollisionEvent> collision_event_subscription;
+
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
-    }
+        collision_event_subscription = EventBus.Subscribe<CollisionEvent>(_OnCollisionUpdated);
 
+    }
+    void _OnCollisionUpdated(CollisionEvent e)
+    {
+        CameraShaker.Instance.ShakeOnce(4f, 4f, 0.1f, 1f); 
+    }
     // Update is called once per frame
     void LateUpdate()
     {
