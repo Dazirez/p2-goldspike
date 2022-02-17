f438f943c61f70a16e924fcb947cdbdf4f8c30c0
Dazirez <dbzheng@umich.edu>
2022-02-17 18:45:33 -0500

    refactor

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,11 +16,10 @@ public class GameController : MonoBehaviour
     public int enemies_left;
     public int score = 0; 
     public int current_level = 0;
-    public int[] num_enemies;
+    public int[] enemies;
     public string[] levels; 
     void Awake()
     {
-        enemies_left = num_enemies[current_level];
         DontDestroyOnLoad(this.gameObject);
         if (instance == null)
         {
@@ -33,44 +32,35 @@ public class GameController : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    damage_event_subscription = EventBus.Subscribe<DamageEvent>(_OnDamageUpdate);
-    //    death_event_subscription = EventBus.Subscribe<ScoreEvent>(_OnScoreUpdated);
-    //}
 
-    
+    private void Update()
+    {
+        enemies_left = GameObject.FindGameObjectsWithTag("enemy").Length;
+        if (enemies_left <= 0)
+        {
+            LoadNext();
+        }
+
+    }
+
     void _OnScoreUpdated(ScoreEvent e)
     {
         score += e.new_score;
         enemies_left--;
-        if (enemies_left <= 0)
-        {
-            enemies_left = num_enemies[current_level % num_enemies.Length];
-            LoadNext();
-        }
     }
     void LoadNext()
     {
         current_level = (current_level + 1) % levels.Length;
         SceneManager.LoadScene(levels[current_level], LoadSceneMode.Single);
-        enemies_left = num_enemies[current_level];
-        Debug.Log("Current level: " + current_level); 
     }
 
     void _OnDamageUpdate(DamageEvent e)
     {
         health -= e.damage_amount;
         enemies_left--;
-        if (enemies_left <= 0)
-        {
-            enemies_left = num_enemies[current_level % num_enemies.Length];
-            LoadNext();
-        }
+        
         if (health <= 0)
         {
-            enemies_left = num_enemies[current_level];
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
