ad9ff4b90528add2afea312cbcd805ad26973549
Dazirez <dbzheng@umich.edu>
2022-02-23 00:28:23 -0500

    witch updates

--- Assets/WitchMovement.cs
+++ Assets/WitchMovement.cs
@@ -7,19 +7,28 @@ public class WitchMovement : Movement
 {
 
 
-
+    Color color; 
+    Animator animator; 
     private float rest_timer = 0;
     private float move_timer = 0;
     private float timer = 0.0f;
+    bool attacking = false; 
+    protected override void Start()
+    {
 
-
+        rest_timer = Random.Range(5.0f, 10.0f);
+        move_timer = Random.Range(2.0f, 5.0f);
+        timer = rest_timer; 
+        animator = GetComponent<Animator>(); 
+        base.Start();
+    }
     protected override void FixedUpdate()
     {
         if (timer >= rest_timer + move_timer)
         {
             timer = 0.0f;
-            rest_timer = Random.Range(0.1f, 2.0f);
-            move_timer = Random.Range(0.1f, 1.0f);
+            rest_timer = Random.Range(5.0f, 10.0f);
+            move_timer = Random.Range(5.0f, 10.0f);
         }
         timer += Time.deltaTime;
         base.FixedUpdate();
@@ -29,8 +38,47 @@ public class WitchMovement : Movement
     {
         if (timer < rest_timer)
         {
+            animator.SetBool("attacking", true);
+            if(!attacking)
+            {
+                ToggleWitchAttack(); 
+            }
             return Vector2.zero;
         }
-        return Vector2.right;
+        else
+        {
+            animator.SetBool("attacking", false);
+            if(attacking)
+            {
+                ToggleWitchAttack(); 
+            }
+
+            return Vector2.right;
+        }
+       
+    }
+
+    public void ToggleWitchAttack()
+    {
+        attacking = !attacking;
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy"); 
+        if(attacking)
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                enemy.GetComponent<Movement>().speed *= 2;
+                color = enemy.GetComponent<SpriteRenderer>().color;
+                enemy.GetComponent<SpriteRenderer>().color = Color.red; 
+            }
+        }
+        else
+        {
+            foreach (GameObject enemy in enemies)
+            {
+                enemy.GetComponent<Movement>().speed /= 2;
+                enemy.GetComponent<SpriteRenderer>().color = color;
+
+            }
+        }
     }
 }
